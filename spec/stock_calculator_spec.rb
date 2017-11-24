9782ae70c39cb1aba1e850dca3cf46bfb0a19190
kibitan <uzukifirst@gmail.com>
2017-11-24 16:38:49 +0100

    implement for negative expression of input date

--- spec/stock_calculator_spec.rb
+++ spec/stock_calculator_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe StockCalculator do
     context 'with valid `stock_symbol` argument' do
       let(:stock_symbol) { 'AAPL' }
 
-      context 'with valid `start_date` argument' do
+      context 'with valid `start_date` argument: string' do
         let(:start_date) { '2017-11-18' }
 
         it 'return StockCalculator::Result' do
@@ -25,6 +25,41 @@ RSpec.describe StockCalculator do
           expect(subject.stock_symbol).to eq 'AAPL'
           expect(subject.start_date).to eq Date.new(2017, 11, 18)
           expect(subject.today).to eq Date.new(2017, 11, 22)
+        end
+      end
+
+      context 'with valid `start_date` argument: date' do
+        let(:start_date) { Date.new(2017, 11, 18) }
+
+        it 'return StockCalculator::Result' do
+          is_expected.to be_instance_of StockCalculator::Result
+          expect(subject.stock_symbol).to eq 'AAPL'
+          expect(subject.start_date).to eq Date.new(2017, 11, 18)
+          expect(subject.today).to eq Date.new(2017, 11, 22)
+        end
+      end
+
+      context 'with invalid `start_date` argument: integer' do
+        let(:start_date) { 12345 }
+
+        it 'raise StockCalculator::Error::InvalidDate' do
+          expect{subject}.to raise_error StockCalculator::Error::InvalidDate
+        end
+      end
+
+      context 'with invalid `start_date` argument: invalid string' do
+        let(:start_date) { 'aaa' }
+
+        it 'raise StockCalculator::Error::InvalidDate' do
+          expect{subject}.to raise_error StockCalculator::Error::InvalidDate
+        end
+      end
+
+      context 'with invalid `start_date` argument: not existed date' do
+        let(:start_date) { '2017-11-31' }
+
+        it 'raise StockCalculator::Error::InvalidDate' do
+          expect{subject}.to raise_error StockCalculator::Error::InvalidDate
         end
       end
     end
--- stock_calculator.rb
+++ stock_calculator.rb
@@ -1,11 +1,39 @@
 class StockCalculator
+  class Error < RuntimeError
+    class InvalidDate < Error; end
+  end
+
   class << self
-    def run(stock_symbol: stock_symbol, start_date: start_date)
-      Result.new(
-        stock_symbol: stock_symbol,
-        start_date: Date.parse(start_date)
-      )
+    def run(stock_symbol:, start_date:)
+      new(stock_symbol: stock_symbol, start_date: start_date).result
     end
+  end
+
+  attr_reader :stock_symbol, :start_date
+
+  def initialize(stock_symbol:, start_date:)
+    @stock_symbol = stock_symbol
+    @start_date = parse_date(start_date)
+  end
+
+  def parse_date(input)
+    case input
+    when String
+      Date.parse(input)
+    when Date
+      input
+    else
+      raise Error::InvalidDate
+    end
+  rescue ArgumentError
+    raise Error::InvalidDate
+  end
+
+  def result
+    Result.new(
+      stock_symbol: stock_symbol,
+      start_date: start_date
+    )
   end
 
   class Result
