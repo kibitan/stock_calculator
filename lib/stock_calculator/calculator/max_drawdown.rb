c54b5bfed90cc8277daa68b201e7d671281b1435
kibitan <uzukifirst@gmail.com>
2017-11-29 18:54:25 +0100

    refactoring StockCalculator::Calculator::RateOfReturn, MaxDrawdown: make immutable

--- lib/stock_calculator/calculator/max_drawdown.rb
+++ lib/stock_calculator/calculator/max_drawdown.rb
@@ -10,9 +10,13 @@ module StockCalculator
 
       class << self
         def calculate(values)
-          new(values).calculate
+          new(values).send(:calculate)
         end
       end
+
+      private
+
+      attr_reader :values
 
       def initialize(values)
         @values = values
@@ -23,12 +27,10 @@ module StockCalculator
         BigDecimal '0.149999466666666666666667e6'
       end
 
-      private
-
       def check_values
-        raise InvalidArgument unless @values.is_a? Array
+        raise InvalidArgument unless values.is_a? Array
 
-        @values.each do |value|
+        values.each do |value|
           raise InvalidArgument unless value.is_a? BigDecimal
           raise NegativeNumber  if value < 0
           raise Zero            if value == 0
--- lib/stock_calculator/calculator/rate_of_return.rb
+++ lib/stock_calculator/calculator/rate_of_return.rb
@@ -9,9 +9,13 @@ module StockCalculator
 
       class << self
         def calculate(initial_value:, final_value:)
-          new(initial_value: initial_value, final_value: final_value).calculate
+          new(initial_value: initial_value, final_value: final_value).send(:calculate)
         end
       end
+
+      private
+
+      attr_reader :initial_value, :final_value
 
       def initialize(initial_value:, final_value:)
         @initial_value = initial_value
@@ -23,20 +27,18 @@ module StockCalculator
         # NOTE:
         #   Rate of return - Wikipedia
         #   https://en.wikipedia.org/wiki/Rate_of_return#Return
-        (@final_value - @initial_value) / @initial_value
+        (final_value - initial_value) / initial_value
       end
 
-      private
-
       def check_values
-        raise InvalidArgument unless @initial_value.is_a? BigDecimal
-        raise InvalidArgument unless @final_value.is_a? BigDecimal
+        raise InvalidArgument unless initial_value.is_a? BigDecimal
+        raise InvalidArgument unless final_value.is_a? BigDecimal
 
-        raise NegativeNumber if @initial_value < 0
-        raise NegativeNumber if @final_value < 0
+        raise NegativeNumber if initial_value < 0
+        raise NegativeNumber if final_value < 0
 
-        raise Zero if @initial_value == 0
-        raise Zero if @final_value == 0
+        raise Zero if initial_value == 0
+        raise Zero if final_value == 0
       end
     end
   end
