ddbae9173feaedb652ebe9597431143e925a3215
kibitan <uzukifirst@gmail.com>
2017-12-04 19:55:36 +0100

    refactoring test

--- spec/notifier/slack_spec.rb
+++ spec/notifier/slack_spec.rb
@@ -1,11 +1,5 @@
 RSpec.describe StockCalculator::Notifier::Slack do
   describe '.notify' do
-    before do
-      allow(StockCalculator::Notifier::Slack::Config).to receive(:webhook_url)
-        .and_return(slack_webhook_url)
-    end
-    let(:slack_webhook_url) { 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX' }
-
     subject do
       StockCalculator::Notifier::Slack.notify(
         text: text,
@@ -13,24 +7,28 @@ RSpec.describe StockCalculator::Notifier::Slack do
       )
     end
 
+    before do
+      allow(StockCalculator::Notifier::Slack::Config).to receive(:webhook_url)
+        .and_return(slack_webhook_url)
+    end
+    let(:slack_webhook_url) { 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX' }
+
+    before do
+      stub_request(:post, request_url)
+        .with(body: { payload: payload_of_request })
+        .to_return(dummy_response_file)
+    end
+    after { WebMock.reset! }
+
     context 'with valid channel' do
       let(:channel) { '#sandbox' }
 
       context 'with valid text' do
         let(:text) { "this is the test test!\nHello World!" }
-        let(:request_url) {  "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX" }
-        let(:dummy_response_file) { File.new('spec/notifier/slack/dummy_responses/success') }
 
-        before do
-          stub_request(:post, request_url)
-            .with(
-              body: {
-                payload: %Q|{"text":"this is the test test!\\nHello World!","channel":"#sandbox"}|
-                }
-              )
-            .to_return(dummy_response_file)
-        end
-        after { WebMock.reset! }
+        let(:request_url) { 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX' }
+        let(:payload_of_request) { %Q|{"text":"this is the test test!\\nHello World!","channel":"#sandbox"}| }
+        let(:dummy_response_file) { File.new('spec/notifier/slack/dummy_responses/success') }
 
         it 'return true' do
           is_expected.to be true
@@ -43,19 +41,9 @@ RSpec.describe StockCalculator::Notifier::Slack do
       let(:channel) { '#invalid' }
       let(:text) { "this is the test test!\nHello World!" }
 
-      let(:request_url) {  "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX" }
+      let(:request_url) { 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX' }
+      let(:payload_of_request) { %Q|{"text":"this is the test test!\\nHello World!","channel":"#invalid"}| }
       let(:dummy_response_file) { File.new('spec/notifier/slack/dummy_responses/invalid_channel') }
-
-      before do
-        stub_request(:post, request_url)
-          .with(
-            body: {
-              payload: %Q|{"text":"this is the test test!\\nHello World!","channel":"#invalid"}|
-              }
-            )
-          .to_return(dummy_response_file)
-      end
-      after { WebMock.reset! }
 
       it 'raise StockCalculator::Notifier::Slack::APIError' do
         expect { subject }.to raise_error StockCalculator::Notifier::Slack::APIError,
