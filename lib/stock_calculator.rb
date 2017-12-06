2dfcb0e2d1e93d33d0959b9fcf3ab112f830beb0
kibitan <uzukifirst@gmail.com>
2017-12-06 22:43:51 +0100

    [WIP] adding verbose option

--- lib/stock_calculator.rb
+++ lib/stock_calculator.rb
@@ -7,8 +7,31 @@ module StockCalculator
   require 'stock_calculator/notifier'
 
   class << self
-    def run(stock_symbol:, start_date:)
-      Main.new(stock_symbol: stock_symbol, start_date: start_date).notify
+    def run(stock_symbol:, start_date:, verbose: false)
+      main = Main.new(stock_symbol: stock_symbol, start_date: start_date)
+      if verbose
+        puts '=' * 5 + 'verbose' + '=' * 5
+        main.result.price_datas.each do |price_data|
+          puts "ticker: #{price_data.ticker}"
+          puts "date: #{price_data.date}"
+          puts "open: #{price_data.open.to_f}"
+          puts "high: #{price_data.high.to_f}"
+          puts "low: #{price_data.low.to_f}"
+          puts "close: #{price_data.close.to_f}"
+          puts "volume: #{price_data.volume.to_f}"
+          puts "ex_dividend: #{price_data.ex_dividend.to_f}"
+          puts "split_ratio: #{price_data.split_ratio.to_f}"
+          puts "adj_open: #{price_data.adj_open.to_f}"
+          puts "adj_high: #{price_data.adj_high.to_f}"
+          puts "adj_low: #{price_data.adj_low.to_f}"
+          puts "adj_close: #{price_data.adj_close.to_f}"
+          puts "adj_volume: #{price_data.adj_volume.to_f}"
+          puts
+        end
+        puts '=' * 5 + 'verbose' + '=' * 5
+        puts
+      end
+      main.notify
     end
   end
 
