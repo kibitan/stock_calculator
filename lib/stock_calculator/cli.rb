a5ad4a25bb5befa8f6d20ad3a4bf4d9e76575b89
kibitan <uzukifirst@gmail.com>
2017-12-07 11:57:31 +0100

    imeplement basic error handling

--- lib/stock_calculator/cli.rb
+++ lib/stock_calculator/cli.rb
@@ -5,6 +5,26 @@ module StockCalculator
     desc 'execute STOCK_SYMBOL START_DATE', 'calculate rate of return, maximum drawdown and notify result'
     def execute(stock_symbol, start_date)
       StockCalculator.run(stock_symbol: stock_symbol, start_date: start_date)
+
+    # Input
+    rescue StockCalculator::InvalidDate
+      $stderr.puts 'invalid start date'
+    rescue StockCalculator::OutOfDate
+      $stderr.puts 'start date should be up to today'
+
+    # Quandl
+    rescue StockCalculator::Quandl::NoAPIKey
+      $stderr.puts 'please set Quandl API key to environment variable QUANDL_API_KEY'
+    rescue StockCalculator::Quandl::NoData
+      $stderr.puts 'No stock data, please check stock symbol/start date is correct'
+    rescue StockCalculator::Quandl::InvalidAPIKey
+      $stderr.puts 'Quandl API key is invalid, please check Quandl API key is correct'
+
+    # Slack
+    rescue StockCalculator::Notifier::Slack::NoWebhookUrl
+      $stderr.puts 'for notifying Slack, please set Incoming Webhook URL to environment variable SLACK_WEBHOOK_URL'
+    rescue StockCalculator::Notifier::Slack::APIError
+      $stderr.puts 'failed to notify Slack, please check Incoming Webhook URL is correct'
     end
   end
 end
--- spec/stock_calculator/cli_spec.rb
+++ spec/stock_calculator/cli_spec.rb
@@ -10,5 +10,90 @@ RSpec.describe StockCalculator::Cli do
 
       expect { subject }.not_to raise_error
     end
+
+    context 'raise StockCalculator::InvalidDate' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::InvalidDate)
+      end
+
+      it do
+        expect { subject }.to output("invalid start date\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'raise StockCalculator::OutOfDate' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::OutOfDate)
+      end
+
+      it do
+        expect { subject }.to output("start date should be up to today\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'raise StockCalculator::Quandl::NoAPIKey' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::Quandl::NoAPIKey)
+      end
+
+      it do
+        expect { subject }.to output("please set Quandl API key to environment variable QUANDL_API_KEY\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'raise StockCalculator::Quandl::NoData' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::Quandl::NoData)
+      end
+
+      it do
+        expect { subject }.to output("No stock data, please check stock symbol/start date is correct\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'raise StockCalculator::Quandl::InvalidAPIKey' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::Quandl::InvalidAPIKey)
+      end
+
+      it do
+        expect { subject }.to output("Quandl API key is invalid, please check Quandl API key is correct\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'raise StockCalculator::Notifier::Slack::NoWebhookUrl' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::Notifier::Slack::NoWebhookUrl)
+      end
+
+      it do
+        expect { subject }.to output("for notifying Slack, please set Incoming Webhook URL to environment variable SLACK_WEBHOOK_URL\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context 'raise StockCalculator::Notifier::Slack::APIError' do
+      before do
+        allow(StockCalculator).to receive(:run)
+          .and_raise(StockCalculator::Notifier::Slack::APIError)
+      end
+
+      it do
+        expect { subject }.to output("failed to notify Slack, please check Incoming Webhook URL is correct\n").to_stderr
+        expect { subject }.not_to raise_error
+      end
+    end
+
   end
 end
